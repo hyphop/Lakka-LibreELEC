252cfe904050cd1810f9ed4786892b367b7557d0
hyphop <art@khadas.com>
2020-08-09 01:19:05 +0800

    amba

--- packages/linux-firmware/wlan-firmware-khadas/package.mk
+++ packages/linux-firmware/wlan-firmware-khadas/package.mk
@@ -4,6 +4,7 @@
 PKG_NAME="wlan-firmware-khadas"
 PKG_VERSION="000"
 PKG_ARCH="any"
+PKG_SHA256="99beb6a0259742ea13c0a8e47e11acbc6066e026abcc3ef9edf92560a6e14f52"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/hyphop/pkg"
 PKG_URL="https://github.com/hyphop/pkg/releases/download/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
@@ -20,5 +21,5 @@ make_target() {
 }
 
 makeinstall_target() {
-  DESTDIR=$INSTALL/usr ./install
+  : # DESTDIR=$INSTALL/usr ./install
 }
