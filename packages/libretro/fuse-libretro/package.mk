3d35cfa6c3e9580a5021b4a0c72d03852ca2ec48
Artem Lapkin <art@khadas.com>
2021-05-31 19:37:58 +0800

    libretro fuse fix arm64
    Signed-off-by: Artem Lapkin <art@khadas.com>

--- packages/libretro/fuse-libretro/package.mk
+++ packages/libretro/fuse-libretro/package.mk
@@ -21,7 +21,7 @@
 PKG_NAME="fuse-libretro"
 PKG_VERSION="e52a9eb"
 PKG_REV="1"
-PKG_ARCH="i386 x86_64 arm"
+PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/fuse-libretro"
 PKG_URL="$PKG_SITE.git"
